ca0ea05f58848b1677ed3e1a42510c47c6c8a1ea
joan19gn <55791773+joan19gn@users.noreply.github.com>
2020-10-09 20:26:46 +0200

    Update app.js

--- web-project/app.js
+++ web-project/app.js
@@ -1,251 +1,142 @@
-const dummyData = require('./dummy-data')
 const express = require('express')
-const app = express()
-const sqlite3 = require('sqlite3')
-const multer = require('multer')
+const bcrypt = require('bcrypt');
+var csrf = require('csurf')
+const saltRounds = 10;
 const expressHandlebars = require('express-handlebars')
-var path = require('path');
 const bodyParser = require('body-parser')
-const { allowedNodeEnvironmentFlags } = require('process')
-const { response } = require('express')
-const { kStringMaxLength } = require('buffer')
+const expressSession = require('express-session')
+var path = require('path');
+const SQLiteStore = require("connect-sqlite3")(expressSession);
+const db = require('./db')
+const blogRouter = require('./routers/blogpost-router')
+const guestbookRouter = require('./routers/guestbook-router')
+const projectRouter = require('./routers/project-router')
+const adminUsername = "annie"
+const adminPassword = "$2b$10$N89UGVWC6eCJdlCme69e4.VxjcOZMfUSaqiqxNemBqwQxK9RzaL0e"
+
+//hashing the password 
+
+/*bcrypt.genSalt(saltRounds, function(err, salt) {
+  bcrypt.hash(adminPassword, salt, function(err, hash) {
+    console.log(hash);
+  });
+});
+*/
+var csrfProtection = csrf()
+
+
+const app = express()
+app.engine('hbs', expressHandlebars({
+  defaultLayout: 'main.hbs',
+}))
+
+app.listen(8080, function () {
+  console.log("Server started on port 8080...");
+});
 
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-app.listen(8080, function(){
-  console.log("Server started on port 8080...");
-});
 
-
-app.engine('hbs', expressHandlebars({
-  defaultLayout: 'main.hbs',
+app.use(expressSession({
+  secret: "dffdfdfgrtggvcdfd",
+  saveUninitialized: false,
+  resave: false,
+  store: new SQLiteStore({
+    db: "sessions.db"
+  })
 }))
-const db = new sqlite3.Database("my-database.db")
 
+app.use(function (request, response, next) {
+  const isLoggedIn = request.session.isLoggedIn
+  response.locals.isLoggedIn = isLoggedIn
+  next()
+})
 
+app.use('/guestbook', guestbookRouter)
+app.use('/blogpost', blogRouter)
+app.use('/project', projectRouter)
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
-const blogposts = [{
-  id: 1,
-  title: "Hello",
-  content: "hello again!"
-},
-{ 
-  id: 2,
-  title: "Annies post",
-  content: "Today.."
-}]
-
-const guestbookposts = [{
-  id: 1,
-  title: "Hello",
-  content: "hello again!"
-}, {
-  id: 2,
-  title: "Annies post",
-  content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-}]
-
-
-app.get('/', function(request, response){
+app.get('/', function (request, response) {
+  const isLoggedIn = request.session.isLoggedIn
   const model = {
-    isLoggedIn: true,
-    title: "Home page"
+    title: "Home page",
+    isLoggedIn
   }
   response.render("home.hbs", model)
 })
 
-app.get('/home', function(request, response) { 
-  const id = request.params.id
-  const blogpost = blogposts.find(
-    h => h.id == id
-  )
-  const model = {
-    blogposts,
-    isLoggedIn: true,
-    title: "Home page"
-  }
-  response.render("home.hbs", model)
-})
 
-app.post('/home', function(request, response){  
-  const model = {
-    blogposts: blogposts,
-    isLoggedIn: true,
-    title: "Home page"
-  }
-  response.render("home.hbs", model)
-})
 
-app.get('/about', function(request, response) {
+app.get('/about', function (request, response) {
+  const isLoggedIn = request.session.isLoggedIn
   const model = {
     title: "About page",
-    isLoggedIn: true
+    isLoggedIn
   }
   response.render('about.hbs', model)
 })
 
-app.get('/contact', function(request, response) {
+app.get('/contact', function (request, response) {
+  const isLoggedIn = request.session.isLoggedIn
   const model = {
     title: "Contact page",
-    isLoggedIn: true
+    isLoggedIn
   }
   response.render('contact.hbs', model)
 })
 
-app.post('/contact', function(request, response){
-  const name = req.body.name
-  const email = req.body.email
+app.get('/error', function(request, response){
+  request.session.isLoggedIn = true
+  response.render('errorMessage.hbs')
+  
 })
 
-app.get('/guestbook', function(request, response) {
-  const id = request.params.id
-  const guestbookpost = guestbookposts.find(
-    h => h.id == id
-  )
+
+app.get('/login', csrfProtection,function (request, response) {
+  const isLoggedIn = request.session.isLoggedIn
   const model = {
-    guestbookposts,
-    title: "Guestbook",
-    isLoggedIn: true,
+    isLoggedIn,
+    layout: false,
+    csrfToken: request.csrfToken(),
   }
-  response.render('guestbook.hbs', model)
-})
-
-app.get('/blogpost/:id', function(request, response) {
-  const id = request.params.id
-  const blogpost = blogposts.find(
-    h => h.id == id
-  )
-  const model = {
-    blogpost,
-    title: "Details",
-    isLoggedIn: true,
-  }
-  response.render('blogpost.hbs', model)
-})
-
-app.post('/guestbook', function(request, response){
-  const title = request.body.title
-  const content = request.body.content
-  const guestbookpost = {
-    id: guestbookposts.length + 1,
-    title, 
-    content,
-  }
-
-  guestbookposts.push(guestbookpost)
-  response.redirect('/guestbook')
-})
-
-app.get('/login', function(request, response) {
-  const model = {
-    isLoggedIn: true,
-    layout: false 
-    }
   response.render('login.hbs', model)
 })
 
-app.get('/admin', function(request, response) {
-  const model = {
-    blogposts: blogposts,
-    title: "Admin page",
-    isLoggedIn: true,
-  }
-  response.render('admin.hbs', model)
+app.post("/login",csrfProtection, function (request, response) {
+  const enteredUsername = request.body.username
+  const enteredPassword = request.body.password
+  const errors = [];
+  if(enteredUsername != adminUsername) {
+    console.log("Wrong username");
+    errors.push("Wrong username")
+   }
+
+
+   bcrypt.compare(enteredPassword, adminPassword, function(err, result){
+    if(result){
+      request.session.isLoggedIn = true;
+      response.redirect("/blogpost/home")
+    }else{
+      console.log("Wrong password");
+      errors.push("Wrong password");
+      request.session.isLoggedIn = false;
+      const model = {
+        layout: false,
+        errors,
+    }
+    response.render('login.hbs', model)
+    }
+   })
 })
 
-app.post('/admin', function(request, response) {
-  const title = request.body.title
-  const content = request.body.content
-  const blogpost = {
-    id: blogposts.length + 1,
-    title, 
-    content,
-  }
-
-  blogposts.push(blogpost)
-  response.redirect('/home')
-})
-
-app.get('/update-posts/:id', function(request, response) {
-  const id = request.params.id
-  const blogpost = blogposts.find(
-    h => h.id == id
-  )
-  const model = {
-    blogpost,
-    isLoggedIn: true,
-    title: "Update page"
-  }
-  console.log(model)
-  response.render('update-posts.hbs', model)
-})
-
-app.post('/update-posts/:id', function(request, response) {
-  const id = request.params.id
-  const newTitle = request.body.title
-  const newContent = request.body.content
-  const blogpost = blogposts.find(
-    h => h.id == id
-  )
-  blogpost.title = newTitle
-  blogpost.content = newContent
-
-  response.redirect("/home")
-})
-
-app.get('/update-gpost/:id', function(request, response) {
-  const id = request.params.id
-  const guestbookpost = guestbookposts.find(
-    h => h.id == id
-  )
-  const model = {
-    guestbookpost,
-    isLoggedIn: true,
-    title: "Update guestbook"
-  }
-  console.log(model)
-  response.render('update-gpost.hbs', model)
-})
-
-app.post('/update-gpost/:id', function(request, response) {
-  const id = request.params.id
-  const newTitle = request.body.title
-  const newContent = request.body.content
-  const guestbookpost = guestbookposts.find(
-    h => h.id == id
-  )
-  guestbookpost.title = newTitle
-  guestbookpost.content = newContent
-
-  response.redirect("/guestbook")
-})
-
-app.post('/delete-posts/:id', function(request, response) {
-  const id = request.params.id
-  const postIndex = blogposts.findIndex(
-    h => h.id == id
-  )
-  blogposts.splice(postIndex, 1)
-  response.redirect('/home')
-})
-
-app.post('/delete-gpost/:id', function(request, response) {
-  const id = request.params.id
-  const gpostIndex = guestbookposts.findIndex(
-    h => h.id == id
-  )
-  guestbookposts.splice(gpostIndex, 1)
-  response.redirect('/guestbook')
-})
-
-app.get('/portfolio', function(request, response) {
-  const model = {
-    title: "Portfolio",
-    isLoggedIn: true
-  }
-  response.render('portfolio.hbs' , model)
+app.post("/logout", function (request, response) {
+  request.session.isLoggedIn = false
+  response.redirect("/blogpost/home")
 })
 
 
